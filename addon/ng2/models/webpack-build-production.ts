9de4f41dc74e40ed2279627772889ae64e0fc77e
Minchenko Valery <valery.vs.va@gmail.com>
2016-09-08 14:55:17 +0300

    Remove compression plugin from webpack production config

--- addon/ng2/models/webpack-build-production.ts
+++ addon/ng2/models/webpack-build-production.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 const WebpackMd5Hash = require('webpack-md5-hash');
-const CompressionPlugin = require('compression-webpack-plugin');
 import * as webpack from 'webpack';
 
 export const getWebpackProdConfigPartial = function(projectRoot: string, appConfig: any) {
@@ -19,13 +18,6 @@ export const getWebpackProdConfigPartial = function(projectRoot: string, appConf
       new webpack.optimize.UglifyJsPlugin(<any>{
         mangle: { screw_ie8 : true, keep_fnames: true },
         compress: { screw_ie8: true }
-      }),
-      new CompressionPlugin({
-          asset: '[path].gz[query]',
-          algorithm: 'gzip',
-          test: /\.js$|\.html$/,
-          threshold: 10240,
-          minRatio: 0.8
       })
     ],
     tslint: {
